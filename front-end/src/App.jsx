21dc9a6feb75b5b438e4b17d50c0bc8940694d17
DatTTSE172775 <datttse172775@fpt.edu.vn>
2024-12-13 15:14:35 +0700

    initialize front-end project with React, Vite, and Chakra UI setup

---
+++ front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { HStack } from "@chakra-ui/react";
+import React from "react";
+
+function App() {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
+}
+
+export default App;
